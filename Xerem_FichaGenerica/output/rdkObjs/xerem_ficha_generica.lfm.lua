2035236667601dbf7ff876a34c2f64dbbf4eab46
Rayk Washington <32473751+RaykWashington@users.noreply.github.com>
2025-01-01 03:05:59 -0300

    teste de modulos
    testando importar modulos em arquivos xml separados
    
    testando tabControl e tabs

--- Xerem_FichaGenerica/output/rdkObjs/xerem_ficha_generica.lfm.lua
+++ Xerem_FichaGenerica/output/rdkObjs/xerem_ficha_generica.lfm.lua
@@ -31,8 +31,18 @@ local function constructNew_frmXeremGenerica()
     obj:setTitle("Xerem Ficha Genérica");
     obj:setName("frmXeremGenerica");
 
+    obj.tabControl1 = GUI.fromHandle(_obj_newObject("tabControl"));
+    obj.tabControl1:setParent(obj);
+    obj.tabControl1:setAlign("client");
+    obj.tabControl1:setName("tabControl1");
+
+    obj.tab1 = GUI.fromHandle(_obj_newObject("tab"));
+    obj.tab1:setParent(obj.tabControl1);
+    obj.tab1:setTitle("Aba 1");
+    obj.tab1:setName("tab1");
+
     obj.layout1 = GUI.fromHandle(_obj_newObject("layout"));
-    obj.layout1:setParent(obj);
+    obj.layout1:setParent(obj.tab1);
     obj.layout1:setLeft(20);
     obj.layout1:setTop(40);
     obj.layout1:setWidth(400);
@@ -68,12 +78,60 @@ local function constructNew_frmXeremGenerica()
     obj.button1:setText("Botão 1");
     obj.button1:setName("button1");
 
+    obj.tab2 = GUI.fromHandle(_obj_newObject("tab"));
+    obj.tab2:setParent(obj.tabControl1);
+    obj.tab2:setTitle("Aba 2");
+    obj.tab2:setName("tab2");
+
+    obj.rectangle1 = GUI.fromHandle(_obj_newObject("rectangle"));
+    obj.rectangle1:setParent(obj.tab2);
+    obj.rectangle1:setAlign("client");
+    obj.rectangle1:setCornerType("round");
+    obj.rectangle1:setColor("grey");
+    obj.rectangle1:setXradius(0.5);
+    obj.rectangle1:setYradius(0.5);
+    obj.rectangle1:setName("rectangle1");
+
+    obj.flowLayout1 = GUI.fromHandle(_obj_newObject("flowLayout"));
+    obj.flowLayout1:setParent(obj.rectangle1);
+    obj.flowLayout1:setAlign("client");
+    obj.flowLayout1:setAutoHeight(true);
+    obj.flowLayout1:setOrientation("horizontal");
+    obj.flowLayout1:setName("flowLayout1");
+
+    obj.label2 = GUI.fromHandle(_obj_newObject("label"));
+    obj.label2:setParent(obj.flowLayout1);
+    obj.label2:setText("anotacoes");
+    obj.label2:setName("label2");
+    obj.label2:setHorzTextAlign("center");
+
+    obj.textEditor1 = GUI.fromHandle(_obj_newObject("textEditor"));
+    obj.textEditor1:setParent(obj.flowLayout1);
+    obj.textEditor1:setAlign("client");
+    obj.textEditor1:setWidth(500);
+    obj.textEditor1:setHeight(800);
+    obj.textEditor1:setName("textEditor1");
+
+    obj.layout2 = GUI.fromHandle(_obj_newObject("layout"));
+    obj.layout2:setParent(obj.flowLayout1);
+    obj.layout2:setAlign("client");
+    obj.layout2:setHeight(100);
+    obj.layout2:setMargins({left = 5, right = 5});
+    obj.layout2:setName("layout2");
+
+    obj.button2 = GUI.fromHandle(_obj_newObject("button"));
+    obj.button2:setParent(obj.layout2);
+    obj.button2:setText("botao");
+    obj.button2:setHorzTextAlign("center");
+    obj.button2:setAlign("client");
+    obj.button2:setName("button2");
+
     obj._e_event0 = obj.button1:addEventListener("onClick",
         function (event)
             local rolagem = Firecast.interpretarRolagem(sheet.valor1);
-            				rolagem = rolagem:concatenar(sheet.valor2);
-            				local mesa = Firecast.getMesaDe(sheet);
-            				mesa.chat:rolarDados(rolagem, " Rolagem de Campo 1");
+            						rolagem = rolagem:concatenar(sheet.valor2);
+            						local mesa = Firecast.getMesaDe(sheet);
+            						mesa.chat:rolarDados(rolagem, " Rolagem de Campo 1");
         end);
 
     function obj:_releaseEvents()
@@ -89,10 +147,19 @@ local function constructNew_frmXeremGenerica()
           self:setNodeDatabase(nil);
         end;
 
-        if self.edit1 ~= nil then self.edit1:destroy(); self.edit1 = nil; end;
+        if self.label2 ~= nil then self.label2:destroy(); self.label2 = nil; end;
+        if self.button2 ~= nil then self.button2:destroy(); self.button2 = nil; end;
+        if self.tabControl1 ~= nil then self.tabControl1:destroy(); self.tabControl1 = nil; end;
+        if self.rectangle1 ~= nil then self.rectangle1:destroy(); self.rectangle1 = nil; end;
+        if self.flowLayout1 ~= nil then self.flowLayout1:destroy(); self.flowLayout1 = nil; end;
         if self.label1 ~= nil then self.label1:destroy(); self.label1 = nil; end;
-        if self.edit2 ~= nil then self.edit2:destroy(); self.edit2 = nil; end;
         if self.button1 ~= nil then self.button1:destroy(); self.button1 = nil; end;
+        if self.textEditor1 ~= nil then self.textEditor1:destroy(); self.textEditor1 = nil; end;
+        if self.tab1 ~= nil then self.tab1:destroy(); self.tab1 = nil; end;
+        if self.edit2 ~= nil then self.edit2:destroy(); self.edit2 = nil; end;
+        if self.layout2 ~= nil then self.layout2:destroy(); self.layout2 = nil; end;
+        if self.edit1 ~= nil then self.edit1:destroy(); self.edit1 = nil; end;
+        if self.tab2 ~= nil then self.tab2:destroy(); self.tab2 = nil; end;
         if self.layout1 ~= nil then self.layout1:destroy(); self.layout1 = nil; end;
         self:_oldLFMDestroy();
     end;
